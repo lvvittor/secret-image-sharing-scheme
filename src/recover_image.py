f4d7e8b257fffcd779b8e3517f3a4c4d09760896
festevezz <festevez@itba.edu.ar>
2023-06-17 15:07:47 -0300

    chore: wip

--- src/recover_image.py
+++ src/recover_image.py
@@ -5,28 +5,60 @@ from z251 import Z251
 import numpy as np
 
 class RecoverImage:
-    def __init__(self, shadows: list[BMPFile], k):
-        self.shadows = shadows
+    def __init__(self, shadows: list[BMPFile], k, shadow_length):
+        self.k = k
         self.shadows_amount = len(shadows)
-        if self.shadows_amount < k:
-            raise ValueError(f"Invalid k value: {k}. At least {k} shadows are required")
+        if self.shadows_amount < self.k:
+            raise ValueError(f"Invalid k value: {self.k}. At least {self.k} shadows are required")
+        
+        # pick k shadows randomly
+        self.shadows = random.sample(shadows, self.k)
+
+        self.block_size = 2 * self.k - 2
+        self.secret_length = shadow_length
+        self.blocks_amount = self.secret_length // self.block_size
+        
+        print("K")
+        print(self.k)
+        print("BLOCK SIZE")
+        print(self.block_size)
+        print("SHADOW LENGTH")
+        print(shadow_length)
+        print("SECRET LENGTH")
+        print(self.secret_length)
+        print("BLOCKS AMOUNT")
+        print(self.blocks_amount)
 
     # Input k shadows, without loss of generality (S1, S2, ..., Sk)
     def recover(self):
         secret_data = []
         # Extract vi,j = (mi,j, di,j), i = 1,2,...,t, j = 1,2,...,k from S1, S2, ..., Sk
         for shadow in self.shadows:
-            mij = []
-            dij = []
+            mi = []
+            di = []
+            row_len = len(shadow.image_data)
+            for i in range(0, self.blocks_amount, 2):
+                # Should I be using LSB here??
+                mi.append(int.from_bytes(shadow.image_data[i // row_len][i % row_len], 'little'))
+                di.append(int.from_bytes(shadow.image_data[i // row_len][(i + 1) % row_len], 'little'))
+            mij.append(mi)
+            dij.append(di)
+            ids.append(shadow.header['reserved1'])
+            
+        # For each group of vi,1, vi,2, ..., vi,k, i = 1,2,...,t, reconstruct fi(x) and gi(x)
+        # from mi,1, mi,2, ..., mi,k and di,1, di,2, ..., di,k using Lagrange interpolation+
 
-            for i in range(0, self.shadows_amount):
-                mij = shadow.image_data[i]
-                dij = shadow.image_data[i + 1]
+        for block in range(0, self.blocks_amount):
+            fi_points = []
+            gi_points = []
+            for i in range(0, self.k):
+                mik: Tuple[Z251, Z251] = (Z251(ids[i]), Z251(mij[i][block]))
+                dik: Tuple[Z251, Z251] = (Z251(ids[i]), Z251(dij[i][block]))
+                fi_points.append(mik)
+                gi_points.append(dik)
 
-            # For each group of vi,1, vi,2, ..., vi,k, i = 1,2,...,t, reconstruct fi(x) and gi(x)
-            # from mi,1, mi,2, ..., mi,k and di,1, di,2, ..., di,k using Lagrange interpolation
-            fi = Polynomial.interpolate(mij)
-            gi = Polynomial.interpolate(dij)
+            fi = Polynomial(points=fi_points)
+            gi = Polynomial(points=gi_points)
 
             # Let ai,0, ai,1, bi,0 and bi,1 be the coefficients of x^0 and x^1 in fi(x) and gi(x) 
             a0 = fi.coefficients[0]
